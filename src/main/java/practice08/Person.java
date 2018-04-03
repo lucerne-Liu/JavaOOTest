56aab5b31c1fd921ddcad879fa9549b8b9d3c59d
Lucerne <lucerne_liu@foxmail.com>
2018-04-03 21:03:24 +0800

    finished practice08

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,63 @@
 package practice08;
 
+import org.fest.assertions.core.AssertionInfo;
+
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        // a quick test to see if the objects are identical
+        if (this == otherObject)
+            return true;
+
+        // must return false if the explicit parameter is null
+        if (otherObject == null)
+            return false;
+
+        // if the classed don't match,they can't be equal
+        if (getClass() != otherObject.getClass())
+            return false;
+
+        Person other = (Person) otherObject;
+        // test whether the fields hava identical values
+        return id == other.getId();
+
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,29 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        final String PREFIX = super.introduce() + " I am a Student. I am ";
+        final String SUFFIX = " Class " + klass.getNumber() + ".";
+        String text = "at";
+        if (this == klass.getLeader()) {
+            text = "Leader of";
+        }
+        return PREFIX + text + SUFFIX;
+    }
 }
