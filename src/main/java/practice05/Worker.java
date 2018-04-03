30ca8a81c9907579c50f3fd6d415b18c7390ba94
Lucerne <lucerne_liu@foxmail.com>
2018-04-03 19:08:42 +0800

    finished practice05

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,11 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+    @Override
+    public String introduce() {
+        return new Person(getName(),getAge()).introduce() + " I am a Worker. I have a job.";
+    }
 }
